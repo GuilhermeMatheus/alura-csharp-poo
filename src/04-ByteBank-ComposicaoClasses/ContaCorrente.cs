35711571593125ec4e20a8f627ce70d0c4b9a42b
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-03 13:49:59 -0300

    Colocamos a ContaCorrente no namespace tb

--- src/04-ByteBank-ComposicaoClasses/ContaCorrente.cs
+++ src/04-ByteBank-ComposicaoClasses/ContaCorrente.cs
@@ -1,46 +1,48 @@
-﻿using _04_ByteBank_ComposicaoClasses;
-
-public class ContaCorrente
+﻿namespace _04_ByteBank_ComposicaoClasses
 {
-    public int numero;
-    public int agencia;
-    public double saldo;
-    public Cliente titular;
-
-    public void Deposita(double valor)
+    public class ContaCorrente
     {
-        this.saldo += valor;
-    }
+        public int numero;
+        public int agencia;
+        public double saldo;
+        public Cliente titular;
 
-    public bool Saca(double valor)
-    {
-        if (this.saldo >= valor)
+        public void Deposita(double valor)
         {
-            this.saldo -= valor;
-            return true;
-        }
-        return false;
-    }
-
-    public bool Transfere(double valor, ContaCorrente outraConta)
-    {
-        // Começamos assim, mas podemos fazer invocando o Saca
-        // e ganhamos um DRY ?
-        
-        //if (this.saldo >= valor)
-        //{
-        //    this.saldo -= valor;
-        //    outraConta.Deposita(valor);
-        //    return true;
-        //}
-
-        bool saqueRealizado = Saca(valor);
-        if (saqueRealizado)
-        {
-            outraConta.Deposita(valor);
-            return true;
+            this.saldo += valor;
         }
 
-        return false;
+        public bool Saca(double valor)
+        {
+            if (this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Transfere(double valor, ContaCorrente outraConta)
+        {
+            // Começamos assim, mas podemos fazer invocando o Saca
+            // e ganhamos um DRY ?
+
+            //if (this.saldo >= valor)
+            //{
+            //    this.saldo -= valor;
+            //    outraConta.Deposita(valor);
+            //    return true;
+            //}
+
+            bool saqueRealizado = Saca(valor);
+            if (saqueRealizado)
+            {
+                outraConta.Deposita(valor);
+                return true;
+            }
+
+            return false;
+        }
     }
+
 }

6ef58e45bebaf0a6856138bed7d9b68ced2cf21e
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-03 12:48:56 -0300

    Criando nossa primeira classe

---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Gabriela";
+            conta.agencia = 123;
+            conta.numero = 123456;
+            conta.saldo = 100;
+
+            Console.WriteLine(conta.titular);
+
+            Console.ReadLine();
+        }
+    }
+}

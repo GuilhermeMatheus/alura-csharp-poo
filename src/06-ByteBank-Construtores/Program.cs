7ed4f2d9e0e17c11d3745db8ed4972610e772a36
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-03 15:04:25 -0300

    Aprendemos construtores

---
+++ src/06-ByteBank-Construtores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank_Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ContaCorrente conta = new ContaCorrente(); dá erro
+            ContaCorrente conta = new ContaCorrente(0, 0);
+
+            Console.ReadLine();
+        }
+    }
+}
